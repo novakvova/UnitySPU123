c3cc49a99198e53a909697d473e3ec073eebb43f
novakvova <novakvova@gmail>
2023-11-18 13:52:10 +0200

    Show basket

--- ApplePicker/Assets/Scripts/ApplePicker.cs
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketButtomY = -14f;
+    public float basketSpacingY = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketButtomY +(basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
     }
 
     // Update is called once per frame
--- ApplePicker/Assets/Scripts/AppleTree.cs
+++ ApplePicker/Assets/Scripts/AppleTree.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //Ўаблон €блука
+    public GameObject applePrefab;
+    //Ўвидк≥сть перем≥щенн€
+    public float speed = 1f;
+    //¬≥дстань з л≥в ≥ справа дл€ руху €блун≥
+    public float leftAndRightEdge = 10f;
+    //≤мов≥рн≥сть частоти м≥ни напр€мку руху
+    public float chanceToChangeDirections = 0.1f;
+    //„астота скиданн€ €блук
+    public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DropApple", 2);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = this.transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
